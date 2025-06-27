8e413a8574883a5b480f4df8a99d1bd94e720a99
Santhosh2141 <snthshsrinivas@gmail.com>
2025-06-27 13:38:04 +0530

    Initial Commit

---
+++ AisleProject/AisleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AisleProjectApp.swift
+//  AisleProject
+//
+//  Created by Santhosh Srinivas on 27/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AisleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
